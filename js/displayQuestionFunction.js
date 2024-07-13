acc89b05ed5966fd86795ee5575058534f098bae
ziad <ziad.nouh111@gmail.com>
2024-07-13 22:25:09 +0300

    styled the radio div, handled the whole div clicking

--- js/displayQuestionFunction.js
+++ js/displayQuestionFunction.js
@@ -7,7 +7,8 @@ function displayQuestion(
   answerInputFields
 ) {
   if (currentQuestionIndex < arrQuestion.length && currentQuestionIndex >= 0) {
-    title.innerHTML = arrQuestion[currentQuestionIndex];
+    // Wrap the question title in a Bootstrap-styled element
+    title.innerHTML = `<div class="mb-3"><h4">${arrQuestion[currentQuestionIndex].title}</h4></div>`;
 
     answersDiv.innerHTML = ""; // Clear previous answers
 
@@ -20,7 +21,9 @@ function displayQuestion(
       radio.id = answerId;
       radio.value = answer;
       radio.setAttribute("class", "styled-radio");
-      radio.classList.add("answer");
+      radio.classList.add("answer", "form-check-input"); // Add Bootstrap class for form input
+
+      // Check if the answer was previously selected
       if (localStorage.getItem(currentQuestionIndex) == radio.value) {
         radio.checked = true;
       }
@@ -29,16 +32,24 @@ function displayQuestion(
       label.setAttribute("class", "labelAnswer");
       label.htmlFor = answerId;
       label.textContent = answer;
-      label.setAttribute("class", "styled-label");
+      label.setAttribute("class", "styled-label form-check-label"); // Add Bootstrap class for form label
 
       let div = document.createElement("div");
-      div.setAttribute("class", "continerOfRadioAndLabel");
+      div.setAttribute("class", "continerOfRadioAndLabel form-check mb-2 ps-5"); // Add Bootstrap class for spacing
 
-      div.appendChild(label);
       div.appendChild(radio);
+      div.appendChild(label);
 
       answersDiv.appendChild(div);
 
+      div.addEventListener("click", () => {
+        radio.checked = true;
+        updateAnswersArray(
+          currentQuestionIndex,
+          selectedAnswers,
+          answerInputFields
+        );
+      });
       // Add event listener to update selectedAnswers when a radio button is checked
       radio.addEventListener("change", () => {
         updateAnswersArray(
