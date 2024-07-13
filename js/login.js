d404fe507f942fdb894b37c5e4300ad3d7f6ed03
ziad <ziad.nouh111@gmail.com>
2024-07-13 21:14:36 +0300

    fixed location.replace func

--- js/login.js
+++ js/login.js
@@ -1,22 +1,26 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const email = document.getElementById('email').value.trim();
-    const password = document.getElementById('password').value;
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
 
-    const storedUserData = JSON.parse(localStorage.getItem('userData'));
+    const storedUserData = JSON.parse(localStorage.getItem("userData"));
 
     if (!storedUserData) {
-        alert('No registered user found.');
-        return;
+      alert("No registered user found.");
+      return;
     }
 
-    if (email === storedUserData.email && password === storedUserData.password) {
-         window.location.href("../exam.html");
-        
-        // Redirect to the Quiz page 
+    if (
+      email === storedUserData.email &&
+      password === storedUserData.password
+    ) {
+      location.replace("../exam.html");
+
+      // Redirect to the Quiz page
     } else {
-        alert('Invalid email or password.');
+      alert("Invalid email or password.");
     }
-});
-
+  });
